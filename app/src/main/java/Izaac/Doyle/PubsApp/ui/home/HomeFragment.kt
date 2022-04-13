3b47ca813f6a344a36ae1da64c9eab4e58cf8832
Izaac Doyle <34064518+IzaacDoyle@users.noreply.github.com>
2022-04-13 18:33:44 +0100

    12Pubs Group Fragment

--- app/src/main/java/Izaac/Doyle/PubsApp/ui/home/HomeFragment.kt
+++ app/src/main/java/Izaac/Doyle/PubsApp/ui/home/HomeFragment.kt
@@ -4,16 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import Izaac.Doyle.PubsApp.R
-import Izaac.Doyle.PubsApp.activities.RuleAdd
 import Izaac.Doyle.PubsApp.databinding.FragmentHomeBinding
 import android.content.Intent
 import android.widget.Toast
-import com.google.android.gms.common.internal.safeparcel.SafeParcelable.Class
 
 class HomeFragment : Fragment() {
 
